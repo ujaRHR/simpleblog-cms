8207fb7f9d4d38dc0c30ec5e81763ee81cdf2869
ujaRHR <rhraju.dev@gmail.com>
2025-08-04 20:21:12 +0600

    added: Comment model associations

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,18 @@
 import User from "./user.model.ts";
 import Post from "./post.model.ts";
+import Comment from "./comment.model.ts";
 
-// Model associations
+// User model associations
 User.hasMany(Post, { foreignKey: "authorId" });
+
+// Post model associations
 Post.belongsTo(User, { as: "author", foreignKey: "authorId" });
 
+// Comment model associations
+Comment.belongsTo(User, { as: "author", foreignKey: "authorId" });
+Comment.belongsTo(Post, { as: "post", foreignKey: "postId" });
+Comment.belongsTo(Comment, { as: "parent", foreignKey: "parentId" });
+Comment.hasMany(Comment, { as: "replies", foreignKey: "parentId" });
+
 // exportting models
-export { User, Post };
+export { User, Post, Comment };
